162d8e393bd9cbd04700c88bbf266f60cc38f6ae
Quentin Benard Diarra <quentin.benarddiarra@gmail.com>
2021-12-10 23:21:40 +0100

    refacto

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// Exo 1 GarçonFille
+/* Exo 1 GarçonFille
+Pour chaque nombre n de la liste, on veut effectuer les opérations suivantes :
+
+si le nombre est divisible par 3 : on affiche Garçon
+si le nombre est divisible par 5 : on affiche Fille
+si le nombre est divisible par 3 et par 5 : on affiche GarçonFille
+sinon : on affiche le nombre n */
 
 const algo = (n) => {
     if (n % 3 === 0 && n % 5 === 0) {
@@ -12,27 +18,35 @@ const algo = (n) => {
     }
   }
   
-  // Exo date 
+/* Exo date 
+  Pour chaque date n dans une liste, on veut avoir ces résultats affichés en fonction des dates de la liste :
+
+la date qui correspond au dernier jour du mois précédant
+la date avec les heures et minutes mises à 0
+la date au format DD/MM/YYYY hh:mm
+le nombre de la semaine dans l’année */
+
   let someDate = new Date();
-  
-  //    => Return last day of previous month ++ hours and minutes reset
-  const getLastDayOfPrevMonth = (date) => new Date(date.getFullYear(), date.getMonth(),0,0);
-  
-  //    => return date with hours and minute reset
-  const resetTime = (date) => new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0)
-  
-  //    => date DD/MM/YYYY hh:mm
-  const formatDate = (date) => `${someDate.getDate()}/${someDate.getMonth()+1}/${someDate.getFullYear()} ${someDate.getHours()}:${someDate.getMinutes()}`
-  
-  //    => nb weeks in year
-  const getWeekNum = (date) => {
-    const lastDayOfYear = new Date(date.getFullYear(), 11, 31)
-    const firstDayOfYear = new Date(someDate.getFullYear(), 0, 1)
-    const msInADay = (24 * 60 * 60 * 1000)
-    return Math.ceil((Math.floor(lastDayOfYear - firstDayOfYear) / msInADay) / 7)
+
+  const timeMachine = (date) => {
+    //    => last day of previous month ++ hours and minutes reset
+    const lastDayOfPrevMonth = new Date(date.getFullYear(), date.getMonth(),0,0);
+
+    //    => reset hours & mins
+    const resetTime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 0, 0)
+
+    //    => date DD/MM/YYYY hh:mm
+    const formatDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+
+    //    => nb weeks in year
+    const getWeekNum = (date) => {
+        const lastDayOfYear = new Date(date.getFullYear(), 11, 31)
+        const firstDayOfYear = new Date(date.getFullYear(), 0, 1)
+        const msInADay = (24 * 60 * 60 * 1000)
+        return Math.ceil((Math.floor(lastDayOfYear - firstDayOfYear) / msInADay) / 7)
+      }
+    
+    return {lastDayOfPrevMonth, resetTime, formatDate, weekNum: getWeekNum(date)}
   }
-  
-  console.log('get last day of previous month', getLastDayOfPrevMonth(someDate))
-  console.log('reset', resetTime(someDate))
-  console.log('format', formatDate(someDate))
-  console.log('week num of year', getWeekNum(someDate))
+
+console.log('time machine', timeMachine(someDate))
